1701454d92545c15d3b261e29863d673e98644ba
Facundo Cejas <facundo.cejas@mi.unc.edu.ar>
2023-12-18 18:07:20 -0300

    Finished project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'getting-started';
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,11 +1,16 @@
-<app-top-bar></app-top-bar>
+import { Component } from '@angular/core';
 
-<div class="container">
-  <router-outlet></router-outlet>
-</div>
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html'
+})
+export class TopBarComponent {
 
-<!-- 
+}
+
+
+/*
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at https://angular.io/license
--->
+*/
